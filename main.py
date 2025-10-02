81e6656d3fdc128272e952464526d93f47077840
Yueil Kaleab <yueildani03@gmail.com>
2025-10-02 23:13:57 +0200

    initial commit - projektstruktur för chess-python

---
+++ main.py
@@ -0,0 +1,8 @@
+from src.chess_game.game import ChessGame
+
+def main():
+    game = ChessGame()
+    game.run_cli()
+
+if __name__ == "__main__":
+    main()
---
+++ src/chess_game/game.py
@@ -0,0 +1,8 @@
+
+class ChessGame:
+    def __init__(self):
+        print("ChessGame initialized")
+
+    def run_cli(self):
+        print("Welcome to Chess!")
+        print("Game loop will go here...")
