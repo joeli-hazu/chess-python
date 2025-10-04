5023fe7fcbae7f427e7e42c296ed30cae78923cf
Yueil Kaleab <yueildani03@gmail.com>
2025-10-04 23:02:58 +0200

    Added full project structure with folders and placeholder files

--- main.py
+++
@@ -1,8 +0,0 @@
-from src.chess_game.game import ChessGame
-
-def main():
-    game = ChessGame()
-    game.run_cli()
-
-if __name__ == "__main__":
-    main()
--- src/chess_game/game.py
+++
@@ -1,8 +0,0 @@
-
-class ChessGame:
-    def __init__(self):
-        print("ChessGame initialized")
-
-    def run_cli(self):
-        print("Welcome to Chess!")
-        print("Game loop will go here...")
