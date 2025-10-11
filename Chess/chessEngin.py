d21bfddc2a44d33250ebc2000ab00cd5636f938b
Yueil Kaleab <yueildani03@gmail.com>
2025-10-12 00:46:52 +0200

    andra delen. nu kan de röra sig fritt

--- Chess/chessEngin.py
+++ Chess/chessEngin.py
@@ -28,3 +28,53 @@ class GameState:
 
         self.whiteToMove = True
         self.moveLog = []
+
+    def makeMove(self, move):
+        """
+        Utför ett drag på brädet.
+        Uppdaterar brädet, byter spelare och loggar draget.
+        """
+        self.board[move.startRow][move.startCol] = "--"
+        self.board[move.endRow][move.endCol] = move.pieceMoved
+        self.moveLog.append(move)  # loggar draget för ev. undo
+        self.whiteToMove = not self.whiteToMove  # byt spelare
+
+
+class Move:
+    """
+    Representerar ett drag i spelet.
+    Lagrar start- och slutposition, pjäsen som flyttas och eventuell pjäs som tas.
+    """
+
+    # Dictionaries för att konvertera mellan schacknotation och index
+    ranksToRows = {"1": 7, "2": 6, "3":5, "4":4, "5":3, "6":2, "7": 1, "8":0}
+    rowsToRanks = {v: k for k, v in ranksToRows.items()}
+
+    filesToCols = {"a": 0, "b":1, "c":2, "d":3, "e":4, "f":5, "g":6, "h":7}
+    colsToFiles = {v: k for k, v in filesToCols.items()}
+
+    def __init__(self, startSq, endSq, board):
+        """
+        startSq: tuple (rad, kol) för startposition
+        endSq: tuple (rad, kol) för slutposition
+        board: den aktuella brädbilden (2D-lista)
+        """
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        self.pieceMoved = board[self.startRow][self.startCol]  # pjäsen som flyttas
+        self.pieceCaptured = board[self.endRow][self.endCol]    # eventuell pjäs som tas
+
+    def getChessNotation(self):
+        """
+        Returnerar draget i vanlig schacknotation, t.ex. 'e2e4'.
+        """
+        return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
+
+    def getRankFile(self, r, c):
+        """
+        Konverterar rad/kol till schacknotation.
+        Exempel: (6,4) -> 'e2'
+        """
+        return self.colsToFiles[c] + self.rowsToRanks[r]
--- Chess/chessmain.py
+++ Chess/chessmain.py
@@ -55,11 +55,38 @@ def main():
     loadImages()                                  # laddar bilderna EN gång innan loopen
 
     running = True
+
+    sqSelected = ()
+    # no square is selected, keep track of the last click of the user (tuple: (row, col))
+    playerClicks = [] # keep of player clicks (two tuples: [(6,4)], [4,4]) flyttning
     while running:
         # --- Event-hantering ---
         for e in p.event.get():
             if e.type == p.QUIT:                  # om spelaren stänger fönstret
                 running = False                   # avsluta loopen
+
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos() # (x, y) location of mouse
+                col = location[0]//SQ_SIZE
+                row = location[1]//SQ_SIZE
+                if sqSelected == (row, col): # the user clicked the same square twice 
+                    sqSelected = () # diselect
+                    playerClicks = [] #clear player clicks
+
+                else: # valid click
+                    sqSelected = (row, col)
+                    playerClicks.append(sqSelected) # append for both 1st adn send secend clicks
+
+                if len(playerClicks) == 2: # after 2nd clicks
+                    move = chessEngin.Move(playerClicks[0], playerClicks[1], gs.board)
+                    print(move.getChessNotation())
+                    gs.makeMove(move)
+                    sqSelected = () 
+                    playerClicks = []
+
+
+
+
 
         # --- Rita ut all grafik för spelet ---
         drawGameState(screen, gs)                 # ritar brädet + pjäser
